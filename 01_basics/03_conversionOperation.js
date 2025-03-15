83aeda25875632a1e2113cd0903ee14dae3b1898
Shiv Singh <shivsinghcse@gmail.com>
2025-03-15 19:47:26 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33asgg";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN - Not a Number
+
+// console.log(typeof NaN); // number
+
+// "33" --> 33
+// "33abc" --> NaN
+// true --> 1; false --> 0
+// Number(null) --> 0
+// Number(undefined) --> NaN
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+// "" => false
+// "Shiv" => true
+
+let someNumber = 33;
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
