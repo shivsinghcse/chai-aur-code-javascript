4d5545f0853aa89ee52d98685570147ac9224e6a
Shiv Singh <shivsinghcse@gmail.com>
2025-03-16 03:05:29 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringSomeNumber = String(someNumber);
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+
+// *********************************** Operations ********************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " Shiv";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 3);
+// console.log(1 + "3");
+// console.log("1" + 3 + 3);
+// console.log(1 + 3 + "3");
+
+// console.log(3 + 4 * 5 % 3);
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); // + converts into number
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2; // not recommended
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
