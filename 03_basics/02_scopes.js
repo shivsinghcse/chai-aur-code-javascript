025f19e36bc6734d5267583667ea36a1b884b822
Shiv Singh <shivsinghcse@gmail.com>
2025-03-24 16:52:49 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,9 +6,53 @@ if(true){
     let a = 10;
     const b = 20;
     var c = 30;
-    console.log("inner " ,a);
+    // console.log("inner " ,a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const username = "Hitesh"
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Hitesh";
+    if(username === 'Hitesh'){
+        const website = "Youtube";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//  ++++++++++++++++++++ intresting +++++++++++++++
+
+console.log(addone(5));
+
+
+
+function addone(num){
+    return num + 1;
+}
+
+
+// console.log(addTwo(5)); // ReferenceError: Cannot access 'addTwo' before initialization because of hoisting
+
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(5);
