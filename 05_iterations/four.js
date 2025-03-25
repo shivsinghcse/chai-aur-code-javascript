48967d1576e259edfffdd9692c38accadebed5e7
Shiv Singh <shivsinghcse@gmail.com>
2025-03-25 08:42:10 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+// for(const key in myObject){
+//     console.log(myObject[key]);
+//     // console.log(myObject.key); // undefined
+// }
+
+// const programming = ["js", "rb", "py", "java"];
+// for( const key in programming){
+//     console.log(key);
+//     console.log(programming[key]);
+// }
+
+// Map is not iteratable we can not use for in with Map.
